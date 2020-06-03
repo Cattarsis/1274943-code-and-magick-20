34f5a46e398247a20f42400fa1093e5c048a9c2b
Cattarsis <formalkane@mail.ru>
2020-06-03 20:10:17 +0400

    Исправления: вывод текста и отрисовка одной диаграммы стали функциями, добавлены переменные для отступов

--- js/stat.js
+++ js/stat.js
@@ -5,12 +5,14 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var FONT_GAP = 16;
+var FONT_GAP = 20;
 var BAR_HEIGHT = 150;
 var BAR_MARGIN = 50;
 var BAR_WIDTH = 40;
-var OFFSET_Y = GAP * 3 + FONT_GAP * 3;
-var GISTOFFSET_X = GAP * 4;
+var GISTOFFSET_Y = 50;
+var GISTOFFSET_X = 40;
+var CAPTIONOFFSET_X = 20;
+var CAPTIONOFFSET_Y = 10;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -29,6 +31,14 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var printMultiLine = function (ctx, x, y, str) {
+  var strs = str.split('\n');
+  for (var i = 0; i < strs.length; i++) {
+    ctx.fillText(strs[i], x, y + FONT_GAP * i);
+  }
+  return strs.length;
+};
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -38,20 +48,25 @@ window.renderStatistics = function (ctx, players, times) {
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + FONT_GAP * 2);
+  var printedLines = printMultiLine(ctx, CLOUD_X + CAPTIONOFFSET_X, CLOUD_Y + CAPTIONOFFSET_Y + FONT_GAP, 'Ура вы победили!\nСписок результатов:');
+
   ctx.fillStyle = '#666';
   for (var i = 0; i < players.length; i++) {
     var saturation = Math.floor(Math.random() * 101);
     var currentTime = Math.round(times[i]);
+    var color = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + saturation + '%, 50%)';
 
-    ctx.fillStyle = '#000';
-    ctx.fillText(currentTime, CLOUD_X + GISTOFFSET_X + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_Y + OFFSET_Y + BAR_HEIGHT - GAP - BAR_HEIGHT * currentTime / maxTime);
-
-    ctx.fillStyle = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + saturation + '%, 50%)';
-    ctx.fillRect(CLOUD_X + GISTOFFSET_X + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_Y + OFFSET_Y + BAR_HEIGHT - BAR_HEIGHT * currentTime / maxTime, BAR_WIDTH, BAR_HEIGHT * currentTime / maxTime);
-
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + GISTOFFSET_X + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_Y + OFFSET_Y + BAR_HEIGHT + GAP + FONT_GAP);
+    drowGist(ctx, currentTime, maxTime, CLOUD_X + GISTOFFSET_X, CLOUD_Y + GISTOFFSET_Y + printedLines * FONT_GAP, i, players[i], color);
   }
+};
+
+var drowGist = function (ctx, currenValue, maxValue, x, y, number, title, color) {
+  ctx.fillStyle = '#000';
+  ctx.fillText(currenValue, x + (BAR_WIDTH + BAR_MARGIN) * number, y + BAR_HEIGHT - GAP - BAR_HEIGHT * currenValue / maxValue);
+
+  ctx.fillStyle = color;
+  ctx.fillRect(x + (BAR_WIDTH + BAR_MARGIN) * number, y + BAR_HEIGHT - BAR_HEIGHT * currenValue / maxValue, BAR_WIDTH, BAR_HEIGHT * currenValue / maxValue);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText(title, x + (BAR_WIDTH + BAR_MARGIN) * number, y + BAR_HEIGHT + FONT_GAP);
 };
