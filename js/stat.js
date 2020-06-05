a0bed59c2752a027321722fc38b2c023292185c5
Cattarsis <formalkane@mail.ru>
2020-06-05 09:13:34 +0400

    Исправления: шрифт в одном месте, темного эльфа больше нет

--- js/stat.js
+++ js/stat.js
@@ -32,6 +32,8 @@ var getMaxElement = function (arr) {
 };
 
 var printMultiLine = function (ctx, x, y, str) {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
   var strs = str.split('\n');
   for (var i = 0; i < strs.length; i++) {
     ctx.fillText(strs[i], x, y + FONT_GAP * i);
@@ -43,24 +45,20 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-
   var maxTime = getMaxElement(times);
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
   var printedLines = printMultiLine(ctx, CLOUD_X + CAPTIONOFFSET_X, CLOUD_Y + CAPTIONOFFSET_Y + FONT_GAP, 'Ура вы победили!\nСписок результатов:');
 
-  ctx.fillStyle = '#666';
   for (var i = 0; i < players.length; i++) {
     var saturation = Math.floor(Math.random() * 101);
     var currentTime = Math.round(times[i]);
     var color = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + saturation + '%, 50%)';
 
-    drowGist(ctx, currentTime, maxTime, CLOUD_X + GISTOFFSET_X, CLOUD_Y + GISTOFFSET_Y + printedLines * FONT_GAP, i, players[i], color);
+    drawGist(ctx, currentTime, maxTime, CLOUD_X + GISTOFFSET_X, CLOUD_Y + GISTOFFSET_Y + printedLines * FONT_GAP, i, players[i], color);
   }
 };
 
-var drowGist = function (ctx, currenValue, maxValue, x, y, number, title, color) {
+var drawGist = function (ctx, currenValue, maxValue, x, y, number, title, color) {
   ctx.fillStyle = '#000';
   ctx.fillText(currenValue, x + (BAR_WIDTH + BAR_MARGIN) * number, y + BAR_HEIGHT - GAP - BAR_HEIGHT * currenValue / maxValue);
 
